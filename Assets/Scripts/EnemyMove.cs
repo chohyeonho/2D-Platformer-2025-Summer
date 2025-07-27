517998a172863110ad2e670d7b2a491f3d3a389c
chohyeonho <chohyeno@kookmin.ac.kr>
2025-07-27 17:59:52 +0900

    Update EnemyMove.cs

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-	Rigidbody2D rigid;
+	Rigidbody2D rigid;            // Rigidbody2D 컴포넌트를 담을 변수
+	public int nextMove;          // 이동 방향 (-1: 왼쪽, 0: 정지, 1: 오른쪽)
 
 	void Awake()
 	{
-		rigid = GetComponent<Rigidbody2D>();
+		rigid = GetComponent<Rigidbody2D>();  // Rigidbody2D 컴포넌트 가져오기
+		Think();                              // 초기 이동 방향 설정
 	}
 
 	void FixedUpdate()
 	{
-		rigid.linearVelocity = new Vector2(-1, rigid.linearVelocity.y);
+		// 현재 방향대로 수평 이동. 수직 속도는 유지.
+		rigid.linearVelocity = new Vector2(nextMove, rigid.linearVelocity.y);
+	}
+
+	void Think()
+	{
+		// -1, 0, 1 중 하나를 랜덤으로 선택해 이동 방향 설정
+		nextMove = Random.Range(-1, 2); // 두 번째 인자는 미포함이므로 2까지 아님
 	}
 }
