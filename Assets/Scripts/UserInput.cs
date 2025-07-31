3092024f705b0fa16092b9e79accd7f743b2a532
chohyeonho <chohyeno@kookmin.ac.kr>
2025-07-31 21:38:00 +0900

    inputsystem 클래스 생성

---
+++ Assets/Scripts/UserInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+	// ★ 싱글톤 인스턴스
+	public static UserInput instance;
+
+	// ★ Controls 입력 클래스 (Input Actions 기반)
+	[HideInInspector]
+	public Controls controls;
+
+	// ▶︎ 초기화 처리
+	private void Awake()
+	{
+		// ✓ 싱글톤 인스턴스 등록
+		if (instance == null)
+		{
+			instance = this;
+		}
+
+		// ✓ Controls 인스턴스 생성
+		controls = new Controls();
+	}
+
+	// ▶︎ 오브젝트 활성화 시 입력 활성화
+	private void OnEnable()
+	{
+		controls.Enable();
+	}
+
+	// ▶︎ 오브젝트 비활성화 시 입력 비활성화
+	private void OnDisable()
+	{
+		controls.Disable();
+	}
+}
