25b053abbf5a678cc63cc5c1ac7f3e6bc51fdd8e
chohyeonho <chohyeno@kookmin.ac.kr>
2025-08-02 02:04:55 +0900

    Update IDamageable.cs

--- Assets/Scripts/Interface/IDamageable.cs
+++ Assets/Scripts/Interface/IDamageable.cs
@@ -5,4 +5,7 @@ public interface IDamageable
 {
 	// ★ 데미지를 처리하는 함수 (float 타입 데미지 수치 전달)
 	public void Damage(float damageAmount);
+
+	// ★ 이미 데미지를 받은 상태인지 확인하는 플래그 (중복 데미지 방지용)
+	public bool HasTakenDamage { get; set; }
 }
